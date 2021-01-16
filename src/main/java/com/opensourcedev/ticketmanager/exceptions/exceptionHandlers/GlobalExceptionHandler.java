f23b6844832006d7ab7eeedd62064bde7471bdde
Simon <sajmon2325@gmail.com>
2021-01-16 14:53:20 +0100

    ExceptionHandlers

--- src/main/java/com/opensourcedev/ticketmanager/exceptions/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/opensourcedev/ticketmanager/exceptions/exceptionHandlers/GlobalExceptionHandler.java
@@ -1,7 +1,17 @@
 package com.opensourcedev.ticketmanager.exceptions.exceptionHandlers;
 
+import com.opensourcedev.ticketmanager.exceptions.ChangeTicketException;
+import com.opensourcedev.ticketmanager.model.items.ChangeTicket;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ChangeTicketException.class)
+    public ResponseEntity<ChangeTicket> handleChangeTicketExceptions(WebRequest request, ChangeTicketException exception){
+        
+    }
 }
